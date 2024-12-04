b77d551d006be38be33f4d8f85e2942896524adb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-12-04 22:31:07 +0200

    Introduce Input Types

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -22,7 +22,7 @@ export const resolvers = {
   },
 
   Mutation: {
-    createJob: (_root, { title, description }) => {
+    createJob: (_root, { input: { title, description } }) => {
       const companyId = "FjcJCHJALA4i";
       return createJob({ companyId, title, description });
     }
