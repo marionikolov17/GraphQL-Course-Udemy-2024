6e699ee255738f1d52af71a5d1a8fe9c9262e137
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-12-04 21:37:11 +0200

    Use query variables and fetch job page

--- job-board/client/src/lib/graphql/queries.js
+++ job-board/client/src/lib/graphql/queries.js
@@ -20,3 +20,23 @@ export async function getJobs() {
     const { jobs } = await client.request(query);
     return jobs;
 }
+
+export async function getJob(id) {
+    const query = gql`
+        query($id: ID!) {
+            job(id: $id) {
+                id
+                date
+                title
+                company {
+                    id
+                    name
+                }
+                description
+            }
+        }
+    `;
+
+    const { job } = await client.request(query, { id });
+    return job;
+}
