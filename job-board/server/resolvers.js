6320adef75f4db111236efdebed1a7fc488cf50a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-12-04 17:22:45 +0200

    Fetch data from database for resolvers

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,18 +1,7 @@
+import { getJobs } from "./db/jobs.js";
+
 export const resolvers = {
   Query: {
-    jobs: () => {
-      return [
-        {
-          id: "test-id-1",
-          title: "This Title 1",
-          description: "This Description",
-        },
-        {
-          id: "test-id-2",
-          title: "This Title 2",
-          description: "This Description",
-        },
-      ];
-    },
+    jobs: () => getJobs()
   },
 };
