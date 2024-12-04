aeb8fbddbd657c33db4d3fb461e279e0ddf52a0a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-12-04 17:17:05 +0200

    Apply arrays and non nullability in schema

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,11 +1,18 @@
 export const resolvers = {
-    Query: {
-        job: () => {
-            return {
-                id: "test-id",
-                title: "This Title",
-                description: "This Description"
-            }
-        }
-    }
-}
+  Query: {
+    jobs: () => {
+      return [
+        {
+          id: "test-id-1",
+          title: "This Title 1",
+          description: "This Description",
+        },
+        {
+          id: "test-id-2",
+          title: "This Title 2",
+          description: "This Description",
+        },
+      ];
+    },
+  },
+};
