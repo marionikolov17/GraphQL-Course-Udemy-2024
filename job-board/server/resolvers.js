2b9e7aa5089cb2bd3f365fc3a8dc6017c56e4f8c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-12-04 22:21:19 +0200

    Implement first mutations

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,5 +1,5 @@
 import { GraphQLError } from "graphql";
-import { getCompanyJobs, getJob, getJobs } from "./db/jobs.js";
+import { createJob, getCompanyJobs, getJob, getJobs } from "./db/jobs.js";
 import { getCompany } from "./db/companies.js";
 
 export const resolvers = {
@@ -19,6 +19,13 @@ export const resolvers = {
       return job;
     },
     jobs: () => getJobs()
+  },
+
+  Mutation: {
+    createJob: (_root, { title, description }) => {
+      const companyId = "FjcJCHJALA4i";
+      return createJob({ companyId, title, description });
+    }
   },
 
   Company: {
