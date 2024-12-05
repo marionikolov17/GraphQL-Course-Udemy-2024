ba7b5249daec10e477148256711c2e8cba842e12
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-12-05 15:36:42 +0200

    Add fetch policy to get jobs

--- job-board/client/src/lib/graphql/queries.js
+++ job-board/client/src/lib/graphql/queries.js
@@ -50,7 +50,10 @@ export async function getJobs() {
         }
     `;
 
-    const { data } = await apolloClient.query({ query });
+    const { data } = await apolloClient.query({ 
+        query,
+        fetchPolicy: "network-only"
+    });
     return data.jobs;
 }
 
