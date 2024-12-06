66464b40ff9c5a2ec8867a91a5f6ffa92198c91a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-12-06 22:15:49 +0200

    Setup graphql-ws client

--- chat/client/src/lib/graphql/client.js
+++ chat/client/src/lib/graphql/client.js
@@ -1,7 +1,9 @@
-import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache, split } from '@apollo/client';
+import { createClient as createWsClient } from "graphql-ws";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions"
 import { getAccessToken } from '../auth';
-
-const httpLink = createHttpLink({ uri: 'http://localhost:9000/graphql' });
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
 
 const authLink = new ApolloLink((operation, forward) => {
   const accessToken = getAccessToken();
@@ -13,7 +15,18 @@ const authLink = new ApolloLink((operation, forward) => {
   return forward(operation);
 });
 
+const httpLink = concat(authLink, createHttpLink({ uri: 'http://localhost:9000/graphql' }));
+
+const wsLink = new GraphQLWsLink(createWsClient({
+  url: "ws://localhost:9000/graphql"
+}));
+
 export const apolloClient = new ApolloClient({
-  link: concat(authLink, httpLink),
-  cache: new InMemoryCache(),
+  link: split(isSubscription, wsLink, httpLink),
+  cache: new InMemoryCache(isSubscription, wsLink, httpLink),
 });
+
+function isSubscription(operation) {
+  const definition = getMainDefinition(operation.query);
+  return definition.kind === Kind.OPERATION_TYPE_DEFINITION && definition.operation === OperationTypeNode.SUBSCRIPTION;
+}
